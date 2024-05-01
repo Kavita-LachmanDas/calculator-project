29417c9758e8b0bd4caeb31c1ec11b44140ddbd5
kavita-LachmanDas <159452574+Kavita-LachmanDas@users.noreply.github.com>
2024-05-01 17:41:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let condition = true;
+while (condition) {
+    let answer = await inquirer.prompt([{
+            message: "write your first number",
+            type: "number",
+            name: "firstNumber"
+        },
+        {
+            message: "write your second number",
+            type: "number",
+            name: "secondNumber"
+        },
+        {
+            message: "select your operator",
+            type: "list",
+            name: "operator",
+            choices: ["addition", "multiplication", "subtraction", "division", "modulus", "exponential"],
+        },]);
+    if (answer.operator === "addition") {
+        console.log(parseFloat(answer.firstNumber + answer.secondNumber));
+    }
+    else if (answer.operator === "subtraction") {
+        console.log(answer.firstNumber - answer.secondNumber);
+    }
+    else if (answer.operator === "multiple") {
+        console.log(answer.firstNumber * answer.secondNumber);
+    }
+    else if (answer.operator === "division") {
+        console.log(answer.firstNumber / answer.secondNumber);
+    }
+    else if (answer.operator === "modulus") {
+        console.log(answer.firstNumber % answer.secondNumber);
+    }
+    else if (answer.operator === "exponential") {
+        console.log(answer.firstNumber ** answer.secondNumber);
+    }
+    condition = answer.firstNumber && answer.secondNumber && answer.operator;
+}
